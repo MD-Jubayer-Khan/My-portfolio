d6a8cbecc743f32971d2c42cb76dc36e71b3fee8
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-08-31 17:44:23 +0600

    added whatsapp api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import {
   FaTimes,
   FaGithub,
   FaLinkedin,
+  FaWhatsapp,
 } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
@@ -118,9 +118,10 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href="http://wa.me/01937013572"
+              target="_blank" rel="noreferrer"
             >
-              Email <HiOutlineMail size={30} />
+              Whatsapp <FaWhatsapp size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
