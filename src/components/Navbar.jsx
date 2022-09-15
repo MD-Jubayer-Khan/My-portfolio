8261273444ade91097ec1ea066bf36634f545518
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-09-15 19:52:32 +0600

    added updated resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/16Ts49uMnSXz4VzUZ4BmmGDqK_pW-RaoB/view?usp=sharing'
+              href='https://drive.google.com/file/d/1gyLw-Rvf2nlgsfiJmfdKDuPzwVLnZibl/view?usp=sharing'
               target="_blank" rel="noreferrer"
             >
               Resume <BsFillPersonLinesFill size={30} />
